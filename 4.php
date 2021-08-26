d64930e8d490c814548898a8babf4814166318df
Fisny <bardi.fisniku@gmail.com>
2021-08-26 15:00:14 +0200

    6

---
+++ 4.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà i seguenti dati:
+nome
+cognome
+un array contenente i suoi voti scolastici
+Stampare nome, cognome e la media dei voti di ogni alunno.
+*/
+
+$alunni = [
+    'Classe A' => [
+        [
+            "Nome" => "Bardi",
+            "Cognome" => "Fisniku",
+            'voti' =>[8, 7, 6]
+        ]
+    ],
+    'Classe B' => [
+        [
+            "Nome" => "Dario",
+            "Cognome" => "Rossi",
+            'voti' =>[6, 7, 7]
+        ]
+    ],
+    'Classe c' => [
+        [
+            "Nome" => "Luca",
+            "Cognome" => "Ianni",
+            'voti' =>[8, 8, 7]
+        ]
+    ]
+
+]
+
+?>
---
+++ 6.php
@@ -0,0 +1,7 @@
+/*
+Utilizzare questo array di partenza e
+stampare tutti gli insegnanti in un rettangolo grigio e tutti i PM in un
+rettangolo verde.
+*/
+
+
