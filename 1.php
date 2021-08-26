f2d42dbe01a32c22ebdd2ecb6f2daaffcf15069f
Fisny <bardi.fisniku@gmail.com>
2021-08-26 15:23:10 +0200

    new

--- 1.php
+++ 1.php
@@ -12,4 +12,29 @@ Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55-60
 */
 
+$partite = [
+    [
+        "casa" => "Olimpia Milano",
+        "ospite" => "Cantù",
+        "casaPunti" => 55,
+        "ospitePunti" => 60
+    ],
+    [
+        "casa" => "Virtus Bologna",
+        "ospite" => "Virtus Roma",
+        "casaPunti" => 40,
+        "ospitePunti" => 30
+    ],
+    [
+        "casa" => "F.C. Matera",
+        "ospite" => "Inter",
+        "casaPunti" => 55,
+        "ospitePunti" => 1
+    ]
+];
+
+for($i = 0;$i < count($partite); $i++) {
+    echo "{$partite[$i] ['casa']} - {$partite[$i] ['ospite']} | {$partite[$i] ['casaPunti']} - {$partite[$i] ['ospitePunti']} <br/>";
+}
+
 ?>
--- 6.php
+++ 6.php
@@ -1,7 +1,32 @@
+<?php
 /*
 Utilizzare questo array di partenza e
 stampare tutti gli insegnanti in un rettangolo grigio e tutti i PM in un
 rettangolo verde.
 */
 
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+?>
 
